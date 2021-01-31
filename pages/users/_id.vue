a7d6181dd261d0fc48bf070558c0fc54d08167cd
unknown <hosein.abolhasani1998@gmail.com>
2021-01-31 16:17:43 +0330

    play routing

---
+++ pages/users/_id.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>user with ID : {{ $route.params.id }}</div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    users route
+    <input type="text" v-model="userId" />
+
+    <button @click="onLoadUser">load user</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: ""
+    };
+  },
+  methods: {
+    onLoadUser() {
+      this.$router.push(`/users/${this.userId}`);
+    }
+  }
+};
+</script>
