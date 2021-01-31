f488721a40d4d15884b1eea0b8baaade93dc2220
unknown <hosein.abolhasani1998@gmail.com>
2021-01-31 19:16:13 +0330

    comelete side nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
-    <the-header />
+    <the-header @sidenavToggle="displaySideNav = !displaySideNav" />
+    <the-sidenav :show="displaySideNav" @close="displaySideNav = false" />
     <Nuxt />
   </div>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      displaySideNav: false
+    };
+  }
+};
+</script>
 <style>
 html {
   font-family: "Open Sans", sans-serif;
