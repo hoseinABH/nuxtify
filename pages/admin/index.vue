c7f8011e2b336f96c90d7951687e5fede54cc73b
unknown <hosein.abolhasani1998@gmail.com>
2021-01-31 20:15:55 +0330

    added admin and error layout

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="admin-page">
     <section class="new-post">
-      <button @click="$router.push('/admin/new-post')">Create Post</button>
+      <app-button @click="$router.push('/admin/new-post')"
+        >Create Post</app-button
+      >
     </section>
     <section class="existing-posts">
       <h1>Existing Posts</h1>
